1f231dcbf70074101f4c53dd1b458fc195b462ba
Mikhail Udalov <mikhail.udalov@gmail.com>
2021-12-31 18:20:34 +0000

    Add kotlin build support

---
+++ src/main/java/com/udalmik/shop/kt/CalculatorConstants.kt
@@ -0,0 +1,8 @@
+package com.udalmik.shop.kt
+
+import java.math.RoundingMode
+
+object CalculatorConstants {
+    const val DEFAULT_SCALE = 2
+    val DEFAULT_ROUNDING_MODE = RoundingMode.HALF_EVEN
+}
